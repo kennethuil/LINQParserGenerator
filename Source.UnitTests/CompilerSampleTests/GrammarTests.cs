4aa7403cf35d7e743c11d0750c78d08b6169304f
Kenneth Uildriks <kennethuil@gmail.com>
2011-09-09 12:07:22 -0500

    More cs files to check in

---
+++ Source.UnitTests/CompilerSampleTests/GrammarTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using CompilerSample;
+using Framework.CodeGen.Expressions;
+using Framework.Parsing;
+using NUnit.Framework;
+
+namespace Source.UnitTests.CompilerSampleTests
+{
+    [TestFixture]
+    public class GrammarTests
+    {
+        [Test]
+        public void TestParserProducesAValue()
+        {
+            LanguageGrammar<Expression> g = new LanguageGrammar<Expression>();
+            var expressionHelper = new ExpressionHelper();
+            var classifierGen = new TerminalClassifier<char>(expressionHelper);
+
+        }
+    }
+}
