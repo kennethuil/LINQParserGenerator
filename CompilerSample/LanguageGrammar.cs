41aadd4255cbd5117d4b07b4f646aea2849c3ecd
Kenneth Uildriks <kennethuil@gmail.com>
2011-09-09 12:06:39 -0500

    Adding files

---
+++ CompilerSample/LanguageGrammar.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Framework.CodeGen.Expressions;
+using Framework.Parsing;
+
+namespace CompilerSample
+{
+    public class LanguageGrammar<TValue> : Grammar<char>
+    {
+        // Boilerplate stuff to set up the regex compiler.
+        // TODO: Package this up somewhere.
+        static Func<string, FiniteAutomatonState<char>> _regexCompiler;
+        static RegexCharNFABuilder _regexNFABuilder;
+        static ExpressionHelper _expressionHelper;
+        static LanguageGrammar()
+        {
+            _expressionHelper = new ExpressionHelper();
+            _regexNFABuilder = new RegexCharNFABuilder(_expressionHelper);
+            var expr = _regexNFABuilder.CreateRegexParser("TestRegexCompile");
+            _regexCompiler = expr.Compile();
+        }
+
+        // Symbols and publicly manipulable rules.
+        Terminal<char, string> Identifier = new Terminal<char, string> { Name = "Identifier", InitialState = _regexCompiler("[A-Za-z][A-Za-z0-9]") };
+        NonTerminal<TValue> VariableReference = new NonTerminal<TValue>();
+        private GrammarRule<string, TValue> VariableRefRule;
+
+        public LanguageGrammar()
+        {
+            var rules = new GrammarRule[] {
+                VariableRefRule = GrammarRule<string, TValue>.Create(VariableReference, Identifier)
+            };
+            Init(rules);
+        }
+    }
+}
---
+++ Source.UnitTests/Framework.Parsing.Tests/BooleanExpressionTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using Framework.CodeGen;
+using NUnit.Framework;
+
+namespace Source.UnitTests.Framework.Parsing.Tests
+{
+    [TestFixture]
+    public class BooleanExpressionTests
+    {
+        [Test]
+        public void AndOrCombinations()
+        {
+            var x = Expression.Parameter(typeof(char), "x");
+            var p1 = new PredicateExpression { Predicate = Expression.Equal(x, Expression.Constant('x')) };
+            var p2 = new PredicateExpression { Predicate = Expression.Equal(x, Expression.Constant('y')) };
+            var p = p1.And(p2);
+
+            var p3 = new PredicateExpression { Predicate = Expression.Equal(x, Expression.Constant('z')) };
+            p = p.And(p3);
+            var andp = (AndExpression)p;
+            foreach (var component in andp.Members)
+            {
+                Assert.IsInstanceOf<PredicateExpression>(component);
+            }
+
+            p = p1.Or(p2).Or(p3);
+            var orp = (OrExpression)p;
+            foreach (var component in orp.Members)
+            {
+                Assert.IsInstanceOf<PredicateExpression>(component);
+            }
+        }
+    }
+}
